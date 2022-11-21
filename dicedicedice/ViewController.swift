82869ad8e054a085bc657bde63b074b422b4db75
IrisZZZ <sincostan0607@icloud.com>
2022-11-22 00:02:58 +0800

    Initial Commit

---
+++ dicedicedice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dicedicedice
+//
+//  Created by Iris ZZ on 2022/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
